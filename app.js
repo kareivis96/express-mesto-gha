36cce8c77098d795e4e58c610b2e7f8129336306
Dmitry Klyuchnikov <96Dimian@inbox.ru>
2023-05-16 18:56:19 +0300

    feat: create 13th PW

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello, world!');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+  next();
+});
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.listen(PORT);
