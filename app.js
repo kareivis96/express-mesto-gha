a97abcbb114221b10fe0015250eb3ef2d0cfaafb
Klyuchnikov Dmitry <96Dimian@inbox.ru>
2023-05-20 12:34:05 +0300

    edit 13th PW

--- app.js
+++ app.js
@@ -5,22 +5,17 @@ const bodyParser = require('body-parser');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   req.user = {
-    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '5d8b8592978f8bd833ca8133'
   };
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
-
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
-
 app.listen(PORT);
