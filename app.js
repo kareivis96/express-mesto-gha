846cfb8d5bfba79d2215bdce26022b2531777cb6
Klyuchnikov Dmitry <96Dimian@inbox.ru>
2023-05-20 13:05:12 +0300

    fix: ESlint config

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   req.user = {
-    _id: '5d8b8592978f8bd833ca8133'
+    _id: '5d8b8592978f8bd833ca8133',
   };
   next();
 });
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
 app.listen(PORT);
--- controllers/cards.js
+++ controllers/cards.js
@@ -2,22 +2,22 @@ const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then(cards => res.send({ data: cards }))
-    .catch(err => res.status(500).send({ message: err.message }));
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link })
-    .then(card => res.send({ data: card }))
-    .catch(err => res.status(500).send({ message: err.message }));
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
-    .then(card => res.send({ data: card }))
-    .catch(err => res.status(500).send({ message: err.message }));
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.likeCard = (req, res) => {
@@ -26,9 +26,9 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-  .then(card => res.send({ data: card }))
-  .catch(err => res.status(500).send({ message: err.message }));
-}
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
@@ -36,6 +36,6 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-  .then(card => res.send({ data: card }))
-  .catch(err => res.status(500).send({ message: err.message }));
-}
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
