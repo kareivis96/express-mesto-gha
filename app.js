05f7cd2e9587b4977daf0497f65ef9e52956658c
Klyuchnikov Dmitry <96Dimian@inbox.ru>
2023-06-03 21:11:16 +0300

    fix: edit 15 PW

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
 const { errorHandler } = require('./middlewares/error.middleware');
 const router = require('./routes/index');
 
@@ -8,10 +10,12 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT);
