a6a494d76e0180e9dba05fe915d741093fb328c8
Klyuchnikov Dmitry <96Dimian@inbox.ru>
2023-06-03 21:03:40 +0300

    fix: edit 15 PW

--- routes/cards.routes.js
+++ routes/cards.routes.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const cardsControllers = require('../controllers/cards.controllers');
-const { auth } = require('../middlewares/auth.middleware');
 
-router.get('/', auth, cardsControllers.getCards);
-router.post('/', auth, cardsControllers.createCard);
-router.delete('/:cardId', auth, cardsControllers.deleteCard);
-router.put('/:cardId/likes', auth, cardsControllers.likeCard);
-router.delete('/:cardId/likes', auth, cardsControllers.dislikeCard);
+router.get('/', cardsControllers.getCards);
+router.post('/', cardsControllers.createCard);
+router.delete('/:cardId', cardsControllers.deleteCard);
+router.put('/:cardId/likes', cardsControllers.likeCard);
+router.delete('/:cardId/likes', cardsControllers.dislikeCard);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const cardRouter = require('./cards.routes');
 const authRouter = require('./auth.routes');
 const NotFoundError = require('../utils/errors/NotFoundError');
 const { PAGE_NOT_FOUND } = require('../utils/constants');
+const { auth } = require('../middlewares/auth.middleware');
 
 router.use('/', authRouter);
-router.use('/users', userRouter);
-router.use('/cards', cardRouter);
+router.use('/users', auth, userRouter);
+router.use('/cards', auth, cardRouter);
 router.use('*', (req, res, next) => next(new NotFoundError(PAGE_NOT_FOUND)));
 
 module.exports = router;
