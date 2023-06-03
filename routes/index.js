c5b68a3266bb3e1430530057f9fc0448dc9c1b67
Klyuchnikov Dmitry <96Dimian@inbox.ru>
2023-06-03 22:22:04 +0300

    fix: edit 15 PW

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ const { PAGE_NOT_FOUND } = require('../utils/constants');
 const { auth } = require('../middlewares/auth.middleware');
 
 router.use('/', authRouter);
-router.use('/users', userRouter);
-router.use('/cards', cardRouter);
+router.use('/users', auth, userRouter);
+router.use('/cards', auth, cardRouter);
 router.use('*', (req, res, next) => next(new NotFoundError(PAGE_NOT_FOUND)));
 
 module.exports = router;
